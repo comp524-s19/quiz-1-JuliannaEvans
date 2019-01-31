8c4e7e37509fcb8555557decbcd5c614869c7782
Julianna Evans <tjevans@live.unc.edu>
2019-01-31 18:05:10 +0000

    calculate

--- main.hs
+++ main.hs
@@ -1,13 +1,12 @@
 finalGrade :: [a] -> [b] -> a
-
-finalGrade (x:xs) (y:ys)
+finalGrade [] _ = 
+finalGrade (x:xs) (y:ys) = (top/weight)
 
       top = top + (x[i] * x[i])
       weight = weight + y[i]
 
 where i = length g
 
-return (top/weight)
 
 
       
